0ca0e01470792edd6267b1dc3707c9dbe7718934
name <varunbrahmane30@gmail.com>
2021-07-08 16:24:47 +0530

    [varun] added: calulate notes for change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace ProgramsForNUnitTesting
         static void Main(string[] args)
         {
             Console.WriteLine(" Programs for NUnit Testing. ");
+
+            #region Vending Machine
+           
+            Console.WriteLine("Enter the Amount:");
+            int money = int.Parse(Console.ReadLine()); 
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+            VendingMachine.calculate(money, notes);
+            Console.WriteLine("Total Number of Notes are :" + VendingMachine.total);
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
---
+++ VendingMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramsForNUnitTesting
+{
+    class VendingMachine
+    {
+		// variable declaration
+		static int i = 0;
+		public static int total = 0;
+
+		
+		
+		static int money;
+
+		// implementation
+		public static int calculate(int money, int[] notes)
+		{
+			
+			int rem;
+			if (money == 0)
+			{
+				return -1;
+			}
+			else
+			{
+				if (money >= notes[i])
+				{
+					
+					int calNotes = money / notes[i];
+					rem = money % notes[i];
+					money = rem;
+					total += calNotes;
+                    Console.WriteLine(notes[i]    + "       Notes -> " + calNotes);
+				}
+				i++;
+				return calculate(money, notes);
+			}
+		}
+	}
+}
