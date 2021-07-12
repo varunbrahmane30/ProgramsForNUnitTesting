87db88f1ad94ec6c6d6ee1cca7d586e4b508bd14
name <varunbrahmane30@gmail.com>
2021-07-12 17:53:04 +0530

    [varun] added: Temperature Conversion

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ProgramsForNUnitTesting
             Console.WriteLine(" Programs for NUnit Testing. ");
 
             #region Vending Machine
-           
+
             //Console.WriteLine("Enter the Amount:");
             //int money = int.Parse(Console.ReadLine()); 
             //int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
@@ -18,7 +18,8 @@ namespace ProgramsForNUnitTesting
             //Console.WriteLine("Total Number of Notes are :" + VendingMachine.total);
             #endregion
 
-            PrintDayOfWeek.calDayOfWeek();
+            //PrintDayOfWeek.calDayOfWeek();
+            TempConversion.convertTemperature();
             Console.ReadKey();
         }
     }
---
+++ TempConversion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramsForNUnitTesting
+{
+    class TempConversion
+    {
+        public static void convertTemperature()
+        {
+            // variable Declarations 
+            int check, celsius, fahrenheit;
+
+
+            // ask user for input
+
+            while (true)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Enter 1 :  Celsius to Fahrenheit");
+                Console.WriteLine("Enter 2 :  Fahrenheit to Celsius ");
+                Console.WriteLine("Enter 3 :  Exit");
+                Console.WriteLine("-------------------------------");
+                check = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------");
+                // calculations 
+
+
+
+                if (check == 1)
+                {
+                    Console.WriteLine("Enter temperature in °C: ");
+                    celsius = int.Parse(Console.ReadLine());
+                    int fahrenheitC = (celsius * 9 / 5) + 32;
+                    Console.WriteLine(+celsius + "°C = " + fahrenheitC + "°F");
+                }
+                else if (check == 2)
+                {
+                    Console.Write("Enter temperature in °F: ");
+                    fahrenheit = int.Parse(Console.ReadLine());
+                    int celsiusF = (fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine(+fahrenheit + "°F = " + celsiusF + "°C");
+                }
+                else if (check == 3)
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid input.");
+                }
+            }
+        }
+    }
+}
